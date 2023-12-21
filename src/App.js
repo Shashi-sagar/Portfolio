4c5ef691e3df21291ed86fb188e14071ad69a6c1
shashi-at-514304094524 <shashi.sagar@wizklub.com>
2023-12-21 22:00:36 +0530

    hjhgjy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import shashi from './images/shashi-sagar.jpeg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   <div className="shashi-sagar mt-0 pt-0 row mx-0">
+      <div className="col-6 d-flex justify-content-center align-items-center">
+        <img src={shashi} className="App-logo" alt="logo" />
+      </div>
     </div>
+ 
+    </>
+   
   );
 }
 
